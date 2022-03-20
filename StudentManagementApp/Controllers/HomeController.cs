b2cdbc363f538f10bb5ded1db1de156ef3d1cf2d
Pooja Palaskar <poojapalaskar17@gmail.com>
2022-03-20 22:18:47 +0530

    rrdgcxzgb

--- StudentManagementApp/Controllers/HomeController.cs
+++ StudentManagementApp/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace StudentManagementApp.Controllers
         {
             return View();
         }
-
+        
         public IActionResult Privacy()
         {
             return View();
